0824260f39b8673378adb339fdc0b0feff3e30ac
leejeje <jinwoolee0826@gmail.com>
2023-10-04 23:41:38 +0900

    test

--- app/src/main/java/com/example/first/LoginActivity.java
+++ app/src/main/java/com/example/first/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
