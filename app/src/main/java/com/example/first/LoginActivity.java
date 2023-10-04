5e40d224a1736a1dab665cc0bc2546743dca40b9
leejeje <jinwoolee0826@gmail.com>
2023-10-04 15:51:45 +0900

    test

--- app/src/main/java/com/example/first/LoginActivity.java
+++ app/src/main/java/com/example/first/LoginActivity.java
@@ -2,13 +2,46 @@ package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText Login_ID, Login_PW;
+    private Button Login_Button, Register_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Login_ID = (EditText)findViewById(R.id.editText_ID);
+        Login_PW = (EditText)findViewById(R.id.editText_PW);
+        Login_Button = (Button) findViewById(R.id.button_Login);
+        Register_Button = (Button) findViewById(R.id.button_Register);
+        Login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Id, Password, Name;
+                Id = Login_ID.getText().toString();
+                Password = Login_PW.getText().toString();
+
+                Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_LONG);
+                Intent intent_MainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_MainActivity);
+            }
+        });
+
+        Register_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_RegisterActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent_RegisterActivity);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/first/RegisterActivity.java
+++ app/src/main/java/com/example/first/RegisterActivity.java
@@ -2,13 +2,44 @@ package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText Register_ID, Register_PW, Register_Name;
+    private Button Register_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Register_ID = (EditText)findViewById(R.id.editText_ID_register);
+        Register_PW = (EditText)findViewById(R.id.editText_PW_register);
+        Register_Name = (EditText)findViewById(R.id.editTextText_Name_register);
+        Register_Button = (Button) findViewById(R.id.button_Register_register);
+        Register_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Id, Password, Name;
+                Id = Register_ID.getText().toString();
+                Password = Register_PW.getText().toString();
+                Name = Register_Name.getText().toString();
+
+                Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다", Toast.LENGTH_LONG);
+                Intent intent_LoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent_LoginActivity);
+
+            }
+        });
+
     }
 }
