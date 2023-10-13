96dce70879c54baec16a51a596b1c4fd22fde9da
BuildTools <unconfigured@null.spigotmc.org>
2023-10-13 22:50:48 +0900

    Gidoupgrade

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent_GidoActivity = new Intent(getApplicationContext(), GidoActivity.class);
+                startActivity(intent_GidoActivity);
             }
         });
 
